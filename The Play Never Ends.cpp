d30e5cc161132c36079d5e8a38cfc0e07e67978c
MdMehediHasanRifat1 <rifatahammed2222@gmail.com>
2025-03-01 03:58:28 +0600

    Div2 A

---
+++ The Play Never Ends.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define yes printf("YES\n")
+#define no printf("NO\n")
+int main() {
+	
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+	    ll n;
+	    cin>>n;
+	    if(n%3==1)
+	        yes;
+	    else
+	        no;
+	}
+	return 0;
+
+}
